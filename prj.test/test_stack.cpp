fcdde44ad4f0c75fe3734044df30414067fb25c6
PlatonFPS <PlatonFPS@yandex.ru>
2024-02-05 15:55:06 +0300

    written simple test

--- prj.test/test_stack.cpp
+++ prj.test/test_stack.cpp
@@ -4,11 +4,24 @@
 
 int main() {
   Stack s;
-  std::cout << s.IsEmpty() << '\n';
+  std::cout << (s.IsEmpty() ? "stack is empty" : "stack is not empty") << '\n';
   try {
     s.Top();
   }
   catch (const std::out_of_range& error) {
-    std::cout << error.what();
+    std::cout << error.what() << '\n';
   }
+  s.Pop();
+  Complex a(2, 3);
+  s.Push(a);
+  std::cout << s.Top() << " | expected {2,3}\n";
+  
+  Complex b(3, 4);
+  s.Push(b);
+  std::cout << s.Top() << " | expected {3,4}\n";
+
+  Stack s2(s);
+  std::cout << s.Top() << " | expected {3,4}\n";
+  s.Pop();
+  std::cout << s.Top() << " | expected {2,3}\n";
 }
