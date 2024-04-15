2414ec03097d1cf130268c8cdfe51b22907b4f3b
PlatonFPS <PlatonFPS@yandex.ru>
2024-04-15 15:58:35 +0300

    tine test

--- prj.test/test_stacklstt.cpp
+++ prj.test/test_stacklstt.cpp
@@ -101,91 +101,77 @@ void CopyTest() {
   StackLstT<T> stack3(stack1);
   stack1.Clear();
   FillWithValues<T>(stack1, revValues);
-  CheckValues<T>(stack3, values);
-  CheckValues<T>(stack1, revValues);
+  CHECK(CheckValues<T>(stack3, values));
+  CHECK(CheckValues<T>(stack1, revValues));
 
   FillWithValues(stack1, values);
   stack2 = stack1;
-  CheckStacks<T>(stack1, stack2);
+  CHECK(CheckStacks<T>(stack1, stack2));
 
   FillWithValues(stack1, values);
   stack3 = stack1;
   stack1.Clear();
   FillWithValues(stack1, revValues);
-  CheckValues<T>(stack3, values);
-  CheckValues<T>(stack1, revValues);
+  CHECK(CheckValues<T>(stack3, values));
+  CHECK(CheckValues<T>(stack1, revValues));
 }
 
 TEST_CASE("Copy") {
   CopyTest<Complex>();
 }
 
-//template <typename T>
-//void TimeTest(const T& a, const T& b) {
-//  long long diff = 0;
-//
-//  StackLstT<T> stack1;
-//  for (int i = 0; i < 1000; i++) {
-//    stack1.Push(a);
-//  }
-//  auto start = std::chrono::steady_clock::now();
-//  StackLstT<T> stack2(stack1);
-//  auto end = std::chrono::steady_clock::now();
-//  for (int i = 0; i < 1000; i++) {
-//    CHECK_EQ(stack2.Top(), a);
-//    CHECK_NOTHROW(stack2.Pop());
-//  }
-//  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-//  std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
-//
-//  diff = duration.count();
-//
-//  start = std::chrono::steady_clock::now();
-//  StackLstT<T> stack3(std::move(stack1));
-//  end = std::chrono::steady_clock::now();
-//  for (int i = 0; i < 1000; i++) {
-//    CHECK_EQ(stack3.Top(), a);
-//    CHECK_NOTHROW(stack3.Pop());
-//  }
-//  duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-//  std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
-//
-//  diff -= duration.count();
-//
-//  CHECK(diff > duration.count() * 10);
-//
-//  StackLstT<T> stack4;
-//  for (int i = 0; i < 1000; i++) {
-//    stack4.Push(b);
-//  }
-//  StackLstT<T> stack5;
-//  start = std::chrono::steady_clock::now();
-//  stack5 = stack4;
-//  end = std::chrono::steady_clock::now();
-//  for (int i = 0; i < 1000; i++) {
-//    CHECK_EQ(stack5.Top(), b);
-//    CHECK_NOTHROW(stack5.Pop());
-//  }
-//  duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-//  std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
-//
-//  diff = duration.count();
-//
-//  start = std::chrono::steady_clock::now();
-//  StackLstT<T> stack6 = std::move(stack4);
-//  end = std::chrono::steady_clock::now();
-//  for (int i = 0; i < 1000; i++) {
-//    CHECK_EQ(stack6.Top(), b);
-//    CHECK_NOTHROW(stack6.Pop());
-//  }
-//  duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
-//  std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
-//
-//  diff -= duration.count();
-//
-//  CHECK(diff > duration.count() * 10);
-//}
-//
-//TEST_CASE("time test") {
-//  TimeTest(Complex(1.023, 2), Complex(2.3, 2.32));
-//}
+template <typename T>
+void TimeTest() {
+  std::vector<T> values;
+  CreateValues<T>(values, 10000);
+  long long diff = 0;
+
+  StackLstT<T> stack1;
+  FillWithValues(stack1, values);
+  auto start = std::chrono::steady_clock::now();
+  StackLstT<T> stack2(stack1);
+  auto end = std::chrono::steady_clock::now();
+  CHECK(CheckValues(stack2, values));
+  auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+  std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
+
+  diff = duration.count();
+
+  start = std::chrono::steady_clock::now();
+  StackLstT<T> stack3(std::move(stack1));
+  end = std::chrono::steady_clock::now();
+  CHECK(CheckValues(stack3, values));
+  duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+  std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
+
+  diff -= duration.count();
+
+  CHECK(diff > duration.count() * 10);
+
+  StackLstT<T> stack4;
+  FillWithValues(stack4, values);
+  StackLstT<T> stack5;
+  start = std::chrono::steady_clock::now();
+  stack5 = stack4;
+  end = std::chrono::steady_clock::now();
+  CHECK(CheckValues(stack5, values));
+  duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+  std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
+
+  diff = duration.count();
+
+  start = std::chrono::steady_clock::now();
+  StackLstT<T> stack6 = std::move(stack4);
+  end = std::chrono::steady_clock::now();
+  CHECK(CheckValues(stack6, values));
+  duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+  std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
+
+  diff -= duration.count();
+
+  CHECK(diff > duration.count() * 10);
+}
+
+TEST_CASE("time test") {
+  TimeTest<Complex>();
+}
