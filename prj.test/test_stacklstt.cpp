da3c0c05e81cc24e8803ad3430cf4f546ed041af
PlatonFPS <PlatonFPS@yandex.ru>
2024-04-15 09:43:16 +0300

    stacklstt test update + reports

--- prj.test/test_stacklstt.cpp
+++ prj.test/test_stacklstt.cpp
@@ -10,96 +10,134 @@ static const Complex c(2, 3);
 
 using StackLst = StackLstT<Complex>;
 
+template <class T>
+void InitTest(const T& a, const T& b, const T& c) {
+  StackLst<T> stack1;
+  CHECK_THROWS(stack1.Top());
+  CHECK(stack1.IsEmpty());
+  CHECK_NOTHROW(stack1.Pop());
+  for(int i = 0; i < 10; i += 1) {
+    CHECK_NOTHROW(stack1.Push(a));
+  }
+  for (int i = 0; i < 10; i += 1) {
+    CHECK_EQ(stack1.Top(), a);
+    CHECK_NOTHROW(stack1.Pop());
+  }
+
+  StackLst<T> stack2;
+  for (int i = 0; i < 10; i += 1) {
+    CHECK_NOTHROW(stack2.Push(c));
+  }
+  for (int i = 0; i < 10; i += 1) {
+    CHECK_NOTHROW(stack1.Push(b));
+  }
+
+  for (int i = 0; i < 10; i += 1) {
+    CHECK_EQ(stack2.Top(), c);
+    CHECK_NOTHROW(stack2.Pop());
+  }
+  for (int i = 0; i < 10; i += 1) {
+    CHECK_EQ(stack1.Top(), c);
+    CHECK_NOTHROW(stack1.Pop());
+  }
+
+  StackLst<T> stack3;
+  for (int i = 0; i < 90; i += 1) {
+    switch (i % 3) {
+      case 0:
+        CHECK_NOTHROW(stack3.Push(a));
+        break;
+      case 1:
+        CHECK_NOTHROW(stack3.Push(b));
+        break;
+      case 2:
+        CHECK_NOTHROW(stack3.Push(c));
+        break;
+    }
+  }
+
+  for (int i = 0; i < 90; i += 1) {
+    switch (i % 3) {
+      case 0:
+        CHECK_EQ(stack3.Top(), a);
+        break;
+      case 1:
+        CHECK_EQ(stack3.Top(), b);
+        break;
+      case 2:
+        CHECK_EQ(stack3.Top(), c);
+        break;
+    }
+    CHECK_NOTHROW(stack3.Pop());
+}
+
 TEST_CASE("Initialization") {
-  StackLst stack;
-  CHECK_THROWS(stack.Top());
-  CHECK(stack.IsEmpty());
-  CHECK_NOTHROW(stack.Pop());
-  stack.Push(a);
-  CHECK_NOTHROW(stack.Top());
+  InitTest(Complex(1, 2), Complex(1.23, -2.32),Complex(4.4, 5));
+}
 
-  stack.Pop();
-  for(int i = 0; i < 100; i++) {
-    stack.Push(a);
+void CopiedTest(StackLst& stack1, StackLst& stack2) {
+  stack1.Clear();
+
+  for (int i = 0; i < 100; i += 1) {
+    CHECK_EQ(stack2.Top(), a);
+    CHECK_NOTHROW(stack2.Pop());
   }
 
-  for(int i = 0; i < 100; i++) {
-    CHECK_EQ(stack.Top(), a);
-    CHECK_NOTHROW(stack.Pop());
+  CHECK_NOTHROW(stack1.Push(b));
+  CHECK_THROW(stack2.Top());
+  CHECK_NOTHROW(stack1.Pop());
+}
+
+template <typename T>
+void CopyTest(const T& a, const T& b, const T& c) {
+  StackLst<T> stack1;
+  for (int i = 0; i < 100; i += 1) {
+    CHECK_NOTHROW(stack1.Push(a));
   }
+
+  StackLst<T> stack2(stack1);
+
+  CopiedTest(stack1, stack2);
+
+  for (int i = 0; i < 100; i += 1) {
+    CHECK_NOTHROW(stack1.Push(a));
+  }
+
+  StackLast<T> stack3 = stack1;
+
+  CopiedTest(stack1, stack3);
 }
 
 TEST_CASE("Copy") {
-  StackLst stack;
-  stack.Push(a);
-  stack.Push(b);
-
-  CHECK_EQ(stack.Top(), b);
-  stack.Pop();
-  CHECK_EQ(stack.Top(), a);
-  stack.Push(b);
-
-  StackLst stack2(stack);
-  CHECK_EQ(stack2.Top(), stack.Top());
-  CHECK_EQ(stack2.Top(), b);
-  stack2.Pop();
-  CHECK_FALSE(stack2.Top() == stack.Top());
-  CHECK_EQ(stack2.Top(), a);
-  stack.Pop();
-  CHECK_EQ(stack2.Top(), stack.Top());
-  stack.Push(b);
-
-  stack2 = stack;
-  CHECK_EQ(stack2.Top(), stack.Top());
-  CHECK_EQ(stack2.Top(), b);
-  stack2.Pop();
-  CHECK_FALSE(stack2.Top() == stack.Top());
-  CHECK_EQ(stack2.Top(), a);
-  stack.Pop();
-  CHECK_EQ(stack2.Top(), stack.Top());
-
-  StackLst stack3;
-  for(int i = 0; i < 100; i++) {
-    stack3.Push(a);
-  }
-  StackLst stack4(stack3);
-  for(int i = 0; i < 100; i++) {
-    CHECK_EQ(stack4.Top(), stack3.Top());
-    stack4.Pop();
-    stack3.Pop();
-  }
-
-  for (int i = 0; i < 100; i++) {
-    stack3.Push(a);
-  }
-  stack4 = stack3;
-  for (int i = 0; i < 100; i++) {
-    CHECK_EQ(stack4.Top(), stack3.Top());
-    stack4.Pop();
-    stack3.Pop();
-  }
+  CopyTest(Complex(1.023, 2), Complex(2.3, 2.32),Complex(4.45, -5));
 }
 
-TEST_CASE("time test") {
+void TimeTest(const T& a, const T& b) {
   long long diff = 0;
 
-  StackLst stack1;
-  for (int i = 0; i < 10000; i++) {
+  StackLst<T> stack1;
+  for (int i = 0; i < 1000; i++) {
     stack1.Push(a);
   }
   auto start = std::chrono::steady_clock::now();
-  StackLst stack2(stack1);
+  StackLst<T> stack2(stack1);
   auto end = std::chrono::steady_clock::now();
-  CHECK_EQ(stack2.Top(), a);
+  for (int i = 0; i < 1000; i++) {
+    CHECK_EQ(stack2.Top(), a);
+    CHECK_NOTHROW(stack2.Pop());
+  }
   auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
   std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
 
   diff = duration.count();
 
   start = std::chrono::steady_clock::now();
-  StackLst stack3(std::move(stack1));
+  StackLst<T> stack3(std::move(stack1));
   end = std::chrono::steady_clock::now();
-  CHECK_EQ(stack3.Top(), a);
+  for (int i = 0; i < 1000; i++) {
+    CHECK_EQ(stack3.Top(), a);
+    CHECK_NOTHROW(stack3.Pop());
+  }
   duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
   std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
 
@@ -107,28 +145,38 @@ TEST_CASE("time test") {
 
   CHECK(diff > duration.count() * 10);
 
-  StackLst stack4;
-  for (int i = 0; i < 10000; i++) {
-    stack4.Push(a);
+  StackLst<T> stack4;
+  for (int i = 0; i < 1000; i++) {
+    stack4.Push(b);
   }
-  StackLst stack5;
+  StackLst<T> stack5;
   start = std::chrono::steady_clock::now();
   stack5 = stack4;
   end = std::chrono::steady_clock::now();
-  CHECK_EQ(stack5.Top(), a);
+  for (int i = 0; i < 1000; i++) {
+    CHECK_EQ(stack5.Top(), b);
+    CHECK_NOTHROW(stack5.Pop());
+  }
   duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
   std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
 
   diff = duration.count();
 
   start = std::chrono::steady_clock::now();
-  StackLst stack6 = std::move(stack4);
+  StackLst<T> stack6 = std::move(stack4);
   end = std::chrono::steady_clock::now();
-  CHECK_EQ(stack6.Top(), a);
+  for (int i = 0; i < 1000; i++) {
+    CHECK_EQ(stack6.Top(), b);
+    CHECK_NOTHROW(stack6.Pop());
+  }
   duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
   std::cout << "Time taken by function: " << duration.count() << " microseconds" << std::endl;
 
   diff -= duration.count();
 
   CHECK(diff > duration.count() * 10);
+}
+
+TEST_CASE("time test") {
+  TimeTest(Complex(1.023, 2), Complex(2.3, 2.32));
 }
