c593ddfaf269cc868492655a5b4925951d604b37
PlatonFPS <PlatonFPS@yandex.ru>
2024-05-06 11:49:10 +0300

    added comparison test between queuelst and queuearr

--- prj.app/timing_q/exec/timing_q.cpp
+++ prj.app/timing_q/exec/timing_q.cpp
@@ -9,86 +9,162 @@
 #include<string>
 
 template <class T>
-void CtorTest(int repetitionCount) {
+void CtorTest(const int repetitionCount) {
   for (int i = 0; i < repetitionCount; i += 1) {
     T type();
   }
 }
 
 template <class T>
-void PushTest(int repetitionCount) {
+void CopyCtorTest(const T& copy) {
+  T type(copy);
+}
+template <class T>
+void RValueCopyCtorTest(T& copy) {
+  T type(std::move(copy));
+}
+
+template <class T>
+void AssignmentTest(const T& copy) {
+  T type;
+  type = copy;
+}
+
+template <class T>
+void RValueAssignmentTest(T& copy) {
+  T type;
+  type = std::move(copy);
+}
+
+template <class T>
+void PushTestLst(const int repetitionCount) {
   QueueLstT<T> queue;
   for (int i = 0; i < repetitionCount; i += 1) {
     queue.Push(i);
   }
 }
+  
+template <class T>
+void PushTestArr(const int repetitionCount) {
+  QueueArrT<T> queue;
+  for (int i = 0; i < repetitionCount; i += 1) {
+    queue.Push(i);
+  }
+}
+
+template <class T>
+void PopTestLst(QueueLstT<T>& queue) {
+  while(!queue.IsEmpty()) {
+    queue.Pop();
+  }
+}
+
+template <class T>
+void PopTestArr(QueueArrT<T>& queue) {
+  while(!queue.IsEmpty()) {
+    queue.Pop();
+  }
+}
 
 int main() {
-  Decorator ctor(&CtorTest<QueueLstT<int>>);
-  std::cout << ctor.MeasureTime(1).count() << '\n';
-  std::cout << ctor.MeasureTime(100).count() << '\n';
-  std::cout << ctor.MeasureTime(10000).count() << '\n';
-  std::cout << ctor.MeasureTime(1000000).count() << '\n';
-  std::cout << ctor.MeasureTime(100000000).count() << '\n';
+  const int kMinCount = 1;
+  const int kMaxCount = 1e6;
+  const int kKoef = 10;
 
-  Decorator push(&PushTest<int>);
-  std::cout << push.MeasureTime(1).count() << '\n';
-  std::cout << push.MeasureTime(100).count() << '\n';
-  std::cout << push.MeasureTime(10000).count() << '\n';
-  std::cout << push.MeasureTime(1000000).count() << '\n';
-  std::cout << push.MeasureTime(100000000).count() << '\n';
 
-  /*Timer timer;
-
-  std::cout << "QueueLstT Push function average time test\n";
-
-  int leftRangeBound = 90000;
-  int RightRangeBound = 100000;
-  int partitionCount = 100;
-  int step = (RightRangeBound - leftRangeBound) / partitionCount;
-
-  long double sum = 0;
-  long long count = 0;
-
-  for (int n = leftRangeBound; n <= RightRangeBound; n += step) {
-    QueueLstT<int> queue;
-    timer.Start();
-    for (int i = 0; i < n; i++) {
-      queue.Push(i);
-    }
-    timer.Stop();
-    sum += timer.GetDuration().count() / n;
-    count++;
+  std::cout << "Ctor test\n" << "QueueLstT<int> | QueueArrT<int>\n";
+  Decorator queueLstCtor(&CtorTest<QueueLstT<int>>);
+  Decorator queueArrCtor(&CtorTest<QueueArrT<int>>);
+  for (int i = kMinCount; i <= kMaxCount; i *= kKoef) {
+    std::cout << i << ": ";
+    std::cout << queueLstCtor.MeasureTime(i) << " | ";
+    std::cout << queueArrCtor.MeasureTime(i) << '\n';
   }
 
-  long double elementTime = sum / count;
-  std::cout << "Average time per element: " << elementTime << '\n';
-  std::cout << "Average elemets per second: " << 10e9 / elementTime << '\n';
-
-  std::cout << "QueueArrT Push function average time test\n";
-
-  leftRangeBound = 90000;
-  RightRangeBound = 100000;
-  partitionCount = 100;
-  step = (RightRangeBound - leftRangeBound) / partitionCount;
-
-  sum = 0;
-  count = 0;
-
-  for (int n = leftRangeBound; n <= RightRangeBound; n += step) {
-    QueueArrT<int> queue;
-    timer.Start();
-    for (int i = 0; i < n; i++) {
-      queue.Push(i);
+  std::cout << "Copy ctor test\n" << "QueueLstT<int> | QueueArrT<int>\n";
+  Decorator queueLstCopyCtor(&CopyCtorTest<QueueLstT<int>>);
+  Decorator queueArrCopyCtor(&CopyCtorTest<QueueArrT<int>>);
+  for (int i = kMinCount; i <= kMaxCount; i *= kKoef) {
+    std::cout << i << ": ";
+    QueueLstT<int> queueLst;
+    QueueArrT<int> queueArr;
+    for (int j = 0; j < i; j += 1) {
+      queueLst.Push(j);
+      queueArr.Push(j);
     }
-    timer.Stop();
-    sum += timer.GetDuration().count() / static_cast<double>(n);
-    count++;
+    std::cout << queueLstCopyCtor.MeasureTime(queueLst) << " | ";
+    std::cout << queueArrCopyCtor.MeasureTime(queueArr) << '\n';
   }
 
-  elementTime = sum / count;
-  std::cout << "Average time per element: " << elementTime << '\n';
-  std::cout << "Average elemets per second: " << 10e9 / elementTime << '\n';*/
+  std::cout << "RValue copy ctor test\n" << "QueueLstT<int> | QueueArrT<int>\n";
+  Decorator queueLstRValueCtor(&RValueCopyCtorTest<QueueLstT<int>>);
+  Decorator queueArrRValueCtor(&RValueCopyCtorTest<QueueArrT<int>>);
+  for (int i = kMinCount; i <= kMaxCount; i *= kKoef) {
+    std::cout << i << ": ";
+    QueueLstT<int> queueLst;
+    QueueArrT<int> queueArr;
+    for (int j = 0; j < i; j += 1) {
+      queueLst.Push(j);
+      queueArr.Push(j);
+    }
+    std::cout << queueLstRValueCtor.MeasureTime(queueLst) << " | ";
+    std::cout << queueArrRValueCtor.MeasureTime(queueArr) << '\n';
+  }
+
+  std::cout << "Assignment test\n" << "QueueLstT<int> | QueueArrT<int>\n";
+  Decorator queueLstAssignment(&AssignmentTest<QueueLstT<int>>);
+  Decorator queueArrAssignment(&AssignmentTest<QueueArrT<int>>);
+  for (int i = kMinCount; i <= kMaxCount; i *= kKoef) {
+    std::cout << i << ": ";
+    QueueLstT<int> queueLst;
+    QueueArrT<int> queueArr;
+    for (int j = 0; j < i; j += 1) {
+      queueLst.Push(j);
+      queueArr.Push(j);
+    }
+    std::cout << queueLstAssignment.MeasureTime(queueLst) << " | ";
+    std::cout << queueArrAssignment.MeasureTime(queueArr) << '\n';
+  }
+
+  std::cout << "RValue assignment test\n" << "QueueLstT<int> | QueueArrT<int>\n";
+  Decorator queueLstRValueAssignment(&RValueAssignmentTest<QueueLstT<int>>);
+  Decorator queueArrRValueAssignment(&RValueAssignmentTest<QueueArrT<int>>);
+  for (int i = kMinCount; i <= kMaxCount; i *= kKoef) {
+    std::cout << i << ": ";
+    QueueLstT<int> queueLst;
+    QueueArrT<int> queueArr;
+    for (int j = 0; j < i; j += 1) {
+      queueLst.Push(j);
+      queueArr.Push(j);
+    }
+    std::cout << queueLstRValueAssignment.MeasureTime(queueLst) << " | ";
+    std::cout << queueArrRValueAssignment.MeasureTime(queueArr) << '\n';
+  }
+
+  std::cout << "Push test\n" << "QueueLstT<int> | QueueArrT<int>\n";
+  Decorator queueLstPush(&PushTestLst<int>);
+  Decorator queueArrPush(&PushTestArr<int>);
+  for (int i = kMinCount; i <= kMaxCount; i *= kKoef) {
+    std::cout << i << ": ";
+    std::cout << queueLstPush.MeasureTime(i) << " | ";
+    std::cout << queueArrPush.MeasureTime(i) << '\n';
+  }
+
+  std::cout << "Pop test\n" << "QueueLstT<int> | QueueArrT<int>\n";
+  Decorator queueLstPop(&PopTestLst<int>);
+  Decorator queueArrPop(&PopTestArr<int>);
+  for (int i = kMinCount; i <= kMaxCount; i *= kKoef) {
+    std::cout << i << ": ";
+    QueueLstT<int> queueLst;
+    QueueArrT<int> queueArr;
+    for(int j = 0; j < i; j += 1) {
+      queueLst.Push(j);
+      queueArr.Push(j);
+    }
+    std::cout << queueLstPop.MeasureTime(queueLst) << " | ";
+    std::cout << queueArrPop.MeasureTime(queueArr) << '\n';
+  }
+
 
   return 0;
 }
