18a12f7864cce4f93ee6b7b7769211e95fc5a34e
PlatonFPS <PlatonFPS@yandex.ru>
2024-05-08 16:01:34 +0300

    new iteration of time tst

--- prj.app/timing_q/exec/timing_q.cpp
+++ prj.app/timing_q/exec/timing_q.cpp
@@ -8,12 +8,67 @@
 #include <string>
 #include <map>
 #include <array>
+#include <vector>
 
-const int kDivisionCount = 25;
+const int kDivisionCount = 20;
 
-void PrintResults(const std::map<long long, int>& map,
-                  const long long min, const long long max) {
-  long long interval = (max - min) / kDivisionCount;
+const double minPassingVer = 0.05;
+
+void ProcessResults(const std::map<long long, long long>& map) {
+  if (map.size() < 2) {
+    std::cout << "Insufficient map size" << '\n';
+    return;
+  }
+
+  //filtering extreme values
+  long long maxVer = 0;
+  for (auto& it : map) {
+    maxVer += it.second;
+  }
+
+  long long min = -1;
+  long long max = -1;
+
+  double currVer = 0;
+  std::map<long long, double> ver;
+  auto it = map.begin();
+  while (it != map.end()) {
+    currVer += (double)it->second / maxVer;
+    if (currVer >= minPassingVer && currVer <= 1 - minPassingVer) {
+      ver[it->first] = (double)it->second / maxVer;
+
+      if (min == -1) min = it->first;
+      if(max == -1 || max < it->first) max = it->first;
+    }
+    it++;
+  }
+  
+  //grouping values in intervals
+  int divisionCount = (ver.size() < kDivisionCount ? ver.size() : kDivisionCount);
+  double interval = static_cast<double>(max - min) / divisionCount;
+  std::vector<double> intervals(divisionCount);
+
+  double left = min;
+  double right = min + interval;
+  double sum = 0;
+  int division = 0;
+
+  for (auto& it : ver) {
+    while (it.first > right) {
+      intervals[division] = sum;
+      division++;
+      left += interval;
+      right += interval;
+      sum = 0;
+    }
+    sum += it.second;
+  }
+
+  for (int i = 0; i < intervals.size(); i += 1) {
+    std::cout << "[" << min + i * interval << "; " << min + (i + 1) * interval << "]: " << intervals[i] * 100 << "%\n";
+  }
+
+  /*long long interval = (max - min) / kDivisionCount;
   long long left = min;
   long long right = min + interval;
 
@@ -35,8 +90,9 @@ void PrintResults(const std::map<long long, int>& map,
   sum = 0;
   count = 0;
   left += interval;
-  right += interval;
+  right += interval;*/
 }
+
 
 int main() {
   const int kMinPow = 0;
@@ -49,9 +105,7 @@ int main() {
   for (int i = kMinPow; i <= kMaxPow; i += 1) {
 
     std::cout << "10^" << i << '\n';
-    std::map<long long, int> map;
-    long long min = -1;
-    long long max = -1;
+    std::map<long long, long long> map;
 
     for (int j = 0; j < kRepetitionCount; j++) {
 
@@ -78,13 +132,9 @@ int main() {
       //long long delta = duration.count();
       if (delta <= 0) continue;
       map[delta]++;
-
-      if(min == -1 || min > delta) min = delta;
-
-      if(max == -1 || max < delta) max = delta;
     }
 
-    PrintResults(map, min, max);
+    ProcessResults(map);
 
     count *= kKoef;
   }
